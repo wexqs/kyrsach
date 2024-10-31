f74e969d425acf67adb43b6060c6532a81946fa2
wexqs <pufik2901@mail.ru>
2024-10-31 12:13:08 +0300

    Create Erros.h

---
+++ Erros.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <system_error>
+#include <stdexcept>
+#include <string>
+class crit_err:public std::runtime_error
+{
+public:
+	crit_err(const std::string& s):std::runtime_error(s){}
+};
+class no_crit_err:public std::runtime_error
+{
+public:
+	no_crit_err(const std::string s): std::runtime_error(s){}
+};

143835497db4492789cd4a6357a2e3c222610ebb
wexqs <pufik2901@mail.ru>
2024-10-31 12:12:35 +0300

    Create Connector.h

---
+++ Connector.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <map>
+#include <string>
+#include "Errors.h"
+
+class Connector
+{
+private:
+    std::map<std::string, std::string> data_base;
+public:
+    int connect(std::string base_file = "/home/stud/kyrsach/test/auth.txt");
+    std::map<std::string, std::string> get_data();
+};

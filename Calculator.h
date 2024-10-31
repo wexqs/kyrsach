81e7c43649e09274478e2b5b04b6527fe68f0350
wexqs <pufik2901@mail.ru>
2024-10-31 12:10:31 +0300

    Create Calculator.h

---
+++ Calculator.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+#include <cstdint>
+
+class Calculator
+{
+    uint16_t results;
+public:
+    Calculator(std::vector<uint16_t> input_data);
+    uint16_t send_res();
+};
